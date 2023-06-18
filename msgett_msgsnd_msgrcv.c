d55b99dba3e6af8da88b156ea55747ee41485f60
3milyIT <137009050+3milyIT@users.noreply.github.com>
2023-06-19 00:33:31 +0200

    Update msgett_msgsnd_msgrcv.c

--- msgett_msgsnd_msgrcv.c
+++ msgett_msgsnd_msgrcv.c
@@ -1,3 +1,153 @@
+//MY SOLUTION
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+struct mesg_buffer {
+    long mesg_type;
+    char mesg_text[100];
+} message;
+
+int main()
+{
+    key_t key;
+    int msgqid;
+    //generate a unique key
+    key = ftok("hello", 65);
+
+    //msgget -> create a message queue using the key, 0666|IPC_CREAT is necessary
+    msgqid = msgget(key, 0666 | IPC_CREAT);
+    printf("Message queue has been created. ID: %d\n", msgqid);
+
+    //send a message to the queue
+    message.mesg_type = 1; //set the message type
+    printf("Write message: ");
+    fgets(message.mesg_text, 100, stdin);
+    printf("\n");
+    msgsnd(msgqid, &message, sizeof(message), 0);
+
+    //receive a message from the queue
+    msgrcv(msgqid, &message, sizeof(message), 1, 0);
+    //display the message
+    printf("%s", message.mesg_text);
+
+    msgctl(msgqid, IPC_RMID, NULL);
+
+    return 0;
+}
+
+
+//MY SOLUTION - errors handling
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+struct mesg_buffer {
+    long mesg_type;
+    char mesg_text[100];
+} message;
+
+int main()
+{
+    key_t key;
+    int msgqid;
+    //generate a unique key
+    key = ftok("hello", 65);
+    if (key == -1) {
+        perror("key");
+        exit(1);
+    }
+    //msgget -> create a message queue using the key, 0666|IPC_CREAT is necessary
+    msgqid = msgget(key, 0666 | IPC_CREAT);
+    if (msgqid == -1) {
+        perror("msgqid");
+        exit(1);
+    }
+    printf("Message queue has been created. ID: %d\n", msgqid);
+
+    //send a message to the queue
+    message.mesg_type = 1; //set the message type
+    printf("Write data: ");
+    fgets(message.mesg_text, 100, stdin);
+    printf("\n");
+    msgsnd(msgqid, &message, sizeof(message), 0);
+
+    //receive a message from the queue
+    msgrcv(msgqid, &message, sizeof(message), 1, 0);
+    //display the message
+    printf("%s", message.mesg_text);
+
+    return 0;
+}
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+struct mesg_buffer {
+    long mesg_type;
+    char mesg_text[100];
+} message;
+
+int main()
+{
+    key_t key;
+    int msgqid;
+    //generate a unique key
+    key = ftok("hello", 65);
+
+    //msgget -> create a message queue using the key, 0666|IPC_CREAT is necessary
+    msgqid = msgget(key, 0666 | IPC_CREAT);
+    message.mesg_type = 1; //set the message type
+    printf("Message queue has been created. ID: %d\n", msgqid);
+
+    printf("Write message: ");
+    fgets(message.mesg_text, 100, stdin);
+    printf("\n");
+
+    pid_t pid = fork();
+    if (pid < 0) {
+        perror("fork() function error");
+    }
+    else if (pid > 0) {
+        printf("First process - sending a message. ID: %d\n", getpid());
+        //send a message to the queue
+        msgsnd(msgqid, &message, sizeof(message), 0);
+        printf("Message sent: %s", message.mesg_text);
+    }
+    else {
+        printf("Second process - getting a message. ID: %d\n", getpid());
+        //receive a message from the queue
+        msgrcv(msgqid, &message, sizeof(message), 1, 0);
+        //display the message
+        printf("Message received: %s", message.mesg_text);
+    }
+
+    msgctl(msgqid, IPC_RMID, NULL);
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
 //MOJE ROZWIĄZANIE
 #include <stdio.h>
 #include <stdlib.h>
